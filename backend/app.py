3b76e5be189e11d37496c5c5e67ad3afeef4dd9d
aravind <aravindyadla6@email.com>
2026-08-07 11:45:07 +0530

    fix cors and render deployment

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,28 @@
 from flask import Flask
 from flask_cors import CORS
-
 from routes.jobs import jobs_bp
 from routes.users import users_bp
 from routes.recommendations import recommend_bp
 from database import verify_connection
+import os
 
 print(verify_connection())
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+CORS(app, resources={
+    r"/*": {
+        "origins": [
+            "http://localhost:5173",
+            "https://graph-job-ai.vercel.app"
+        ]
+    }
+})
 
 app.register_blueprint(jobs_bp)
 app.register_blueprint(users_bp)
 app.register_blueprint(recommend_bp)
+
 
 @app.route("/")
 def home():
@@ -22,5 +30,9 @@ def home():
         "message": "Graph Job Recommendation API Running"
     }
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
